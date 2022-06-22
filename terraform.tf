c999fbe60405e2ce518604a25db0a4590821b6ad
Devendra Variya <devendravariya73@gmail.com>
2022-06-22 21:04:20 +0530

    changes

--- terraform.tf
+++ terraform.tf
@@ -47,7 +47,7 @@
       vpc_id = "$(aws_vpc.myvpc.id)"
       route = {
           cidr_block = "0.0.0.0/0 "
-        gateway_id = "${aws_internet_gateway.igw.id}"
+        gateway_id = aws_internet_gateway.igw.id
       }
       tags = {
         "Name" = "Public_rt"
