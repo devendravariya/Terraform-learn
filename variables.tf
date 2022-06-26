88924cafac722ccc2f8d01f9d97497a3dd0bd1ea
Devendra Variya <devendravariya73@gmail.com>
2022-06-26 17:10:04 +0530

    changes

--- variables.tf
+++ variables.tf
@@ -10,6 +10,20 @@ variable "subnets_cidr" {
 }
 variable "azs" {
     type = list
-    default = ["ap-south-1a", "ap-south-1b"]
+    default = ["ap-south-1a", "ap-south-1b"] 
+}
+variable "ami" {
+    dedefault = "ami-0756a1c858554433e" 
+}
+variable "instance_type" {
+    default = "t2.micro"
+}
+variable "azs" {
+    type = list
+    default = ["ap-south-1a", "ap-south-1b  "]
+  
+}
+variable "allowed_cidr_blocks" {
+    default = "0.0.0.0/0"
   
 }
