87ac5bdd9402f862ace60d2747da38f08ede0a1f
Devendra Variya <devendravariya73@gmail.com>
2022-07-02 22:38:27 +0530

    changes

--- Provisioner_Demo/security.tf
+++ Provisioner_Demo/security.tf
@@ -3,38 +3,39 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow TLS inbound traffic"
   vpc_id      = data.aws_vpc.main    #default
 
-ingress = [ {  #rules
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = "0.0.0.0/0"
-},
-{
-    description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = "0.0.0.0/0"
-},
-{
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = "0.0.0.0/0"
-}
-]
-  
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
+  ingress = [ {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "test"
+    from_port = 80
+    ipv6_cidr_blocks = []
+    prefix_list_ids = []
+    protocol = "tcp"
+    security_groups = []
+    self = false
+    to_port = 80
+  },
+  {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "test"
+    from_port = 22
+    ipv6_cidr_blocks = []
+    prefix_list_ids = []
+    protocol = "tcp"
+    security_groups = []
+    self = false
+    to_port = 22
+  }]
+  egress = [ {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "test"
+    from_port = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids = []
+    protocol = "-1"
+    security_groups = []
+    self = false
+    to_port = 0
+  } ]
   tags = {
     Name = "rules"
   }
