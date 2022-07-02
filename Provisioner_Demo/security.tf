26a05b76892d98ed831c753efde9371a10166d20
Devendra Variya <devendravariya73@gmail.com>
2022-07-02 22:54:21 +0530

    change

--- Provisioner_Demo/security.tf
+++ Provisioner_Demo/security.tf
@@ -7,32 +7,21 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks = [ "0.0.0.0/0" ]
     description = "test"
     from_port = 80
-    ipv6_cidr_blocks = []
-    prefix_list_ids = []
     protocol = "tcp"
-    security_groups = []
-    self = false
     to_port = 80
   },
   {
     cidr_blocks = [ "0.0.0.0/0" ]
     description = "test"
     from_port = 22
-    ipv6_cidr_blocks = []
-    prefix_list_ids = []
     protocol = "tcp"
-    security_groups = []
-    self = false
     to_port = 22
   }]
   egress = [ {
     cidr_blocks = [ "0.0.0.0/0" ]
     description = "test"
     from_port = 0
-    ipv6_cidr_blocks = []
-    prefix_list_ids = []
     protocol = "-1"
-    security_groups = []
     self = false
     to_port = 0
   } ]
