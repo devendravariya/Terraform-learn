a3aef135b7e3a8c269e043b1ba70c3dae16e3297
Devendra Variya <devendravariya73@gmail.com>
2022-07-04 21:53:00 +0530

    change

--- Provisioner_Demo/security.tf
+++ Provisioner_Demo/security.tf
@@ -1,23 +1,23 @@
-module "vote_service_sg" {
+module "test" {
   source = "terraform-aws-modules/security-group/aws"
 
   name        = "user-service"
   description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
   vpc_id      = "vpc-e88a5a83"
 
-  ingress_cidr_blocks      = ["0.0.0.0/0"]
-  ingress_rules            = ["ssh-22-tcp"]
+  ingress_cidr_blocks      = ["172.31.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
   ingress_with_cidr_blocks = [
     {
       from_port   = 80
       to_port     = 80
       protocol    = "tcp"
       description = "User-service ports"
-      cidr_blocks = "0.0.0.0/16"
+      cidr_blocks = "0.0.0.0/0"
     },
     {
-      rule        = "postgresql-tcp"
-      cidr_blocks = "0.0.0.0/0"
+      rule        = "ssh-tcp"
+      cidr_blocks = "172.31.0.0/16"
     },
   ]
 }
