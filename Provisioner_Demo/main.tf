c8dda96042e56d2866d48554986f716459ed3e0b
Devendra Variya <devendravariya73@gmail.com>
2022-07-04 22:04:28 +0530

    change

--- Provisioner_Demo/main.tf
+++ Provisioner_Demo/main.tf
@@ -2,7 +2,6 @@
   ami           ="ami-0756a1c858554433e"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.key.key_name}"
-  vpc_security_group_ids = module.test.test_id
   user_data = "${file("./install_apache.sh")}"
   tags = {
     Name = "web-server"
