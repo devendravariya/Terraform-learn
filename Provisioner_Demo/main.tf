6fa28008345dd1a2112cfd31cd633fdf5a77fba7
Devendra Variya <devendravariya73@gmail.com>
2022-07-07 00:11:38 +0530

    change

--- Provisioner_Demo/main.tf
+++ Provisioner_Demo/main.tf
@@ -3,6 +3,21 @@
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.key.key_name}"
   user_data = "${file("./install_apache.sh")}"
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_ips.txt"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "echo ${self.private_ip} >> /home/ubuntu/private_ips.txt"
+    ]
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      host = "${self.public_ip}"
+      private_key = "${file("/root/.ssh/terraform")}"
+        }
+  
+  }
   tags = {
     Name = "web-server"
   }
